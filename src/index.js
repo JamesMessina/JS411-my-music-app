16bff529a94132062848f09ecc01efc4ad4d5ad4
James Messina <jmessina17@gmail.com>
2021-06-22 18:31:21 -0500

    added dependency packages, reformatted app.js, created app nav bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MusicApp from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MusicApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
